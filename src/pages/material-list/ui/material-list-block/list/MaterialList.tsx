0fa66fc8278e57020a9ffc37e4f08f9c764ff580
oceanzove <zove888@gmail.com>
2024-11-13 21:41:34 +0500

    Добавляет MaterialListItem пропсы

--- src/pages/material-list/ui/material-list-block/list/MaterialList.tsx
+++ src/pages/material-list/ui/material-list-block/list/MaterialList.tsx
@@ -3,14 +3,112 @@ import css from './MaterialList.module.scss';
 import { MaterialListItem } from './item';
 
 export const MaterialList = () => {
-	const props = [{ id: 1, item: 'name1' }, { id: 2, item: 'name2' }, { id: 3, item: 'name3' }];
-	const items = props.map(item => (
-		<MaterialListItem key={item.id} />
+	const materials = [
+		{
+			id: 1,
+			title: 'Материал 1',
+			author: 'Автор 1',
+			competencies: ['Аналитика', 'Разработка', 'Коммуникация'],
+			imageUrl: 'https://i.pinimg.com/564x/34/f2/d5/34f2d554f45ac66757795e8bd0443abb.jpg',
+		},
+		{
+			id: 2,
+			title: 'Материал 2',
+			author: 'Автор 2',
+			competencies: ['Креативность', 'Проектирование'],
+			imageUrl: 'https://i.pinimg.com/564x/34/f2/d5/34f2d554f45ac66757795e8bd0443abb.jpg',
+		},
+		{
+			id: 3,
+			title: 'Материал 3',
+			author: 'Автор 3',
+			competencies: ['Лидерство', 'Технические навыки'],
+			imageUrl: 'https://i.pinimg.com/564x/34/f2/d5/34f2d554f45ac66757795e8bd0443abb.jpg',
+		},
+		{
+			id: 4,
+			title: 'Материал 4',
+			author: 'Автор 4',
+			competencies: ['Организация', 'Планирование', 'Управление временем'],
+			imageUrl: 'https://via.placeholder.com/150',
+		},
+		{
+			id: 5,
+			title: 'Материал 5',
+			author: 'Автор 5',
+			competencies: ['Научные исследования', 'Инновации'],
+			imageUrl: 'https://via.placeholder.com/150',
+		},
+		{
+			id: 6,
+			title: 'Материал 6',
+			author: 'Автор 6',
+			competencies: ['Критическое мышление', 'Решение проблем'],
+			imageUrl: 'https://via.placeholder.com/150',
+		},
+		{
+			id: 7,
+			title: 'Материал 7',
+			author: 'Автор 7',
+			competencies: ['Работа в команде', 'Навыки переговоров'],
+			imageUrl: 'https://via.placeholder.com/150',
+		},
+		{
+			id: 8,
+			title: 'Материал 8',
+			author: 'Автор 8',
+			competencies: ['Мотивация', 'Саморазвитие'],
+			imageUrl: 'https://via.placeholder.com/150',
+		},
+		{
+			id: 9,
+			title: 'Материал 9',
+			author: 'Автор 9',
+			competencies: ['Гибкость', 'Адаптивность'],
+			imageUrl: 'https://via.placeholder.com/150',
+		},
+		{
+			id: 10,
+			title: 'Материал 10',
+			author: 'Автор 10',
+			competencies: ['Управление проектами', 'Ресурсное планирование'],
+			imageUrl: 'https://via.placeholder.com/150',
+		},
+		{
+			id: 11,
+			title: 'Материал 11',
+			author: 'Автор 11',
+			competencies: ['Техническое письмо', 'Документация'],
+			imageUrl: 'https://via.placeholder.com/150',
+		},
+		{
+			id: 12,
+			title: 'Материал 12',
+			author: 'Автор 12',
+			competencies: ['Исследование рынка', 'Прогнозирование'],
+			imageUrl: 'https://via.placeholder.com/150',
+		},
+		{
+			id: 13,
+			title: 'Материал 13',
+			author: 'Автор 13',
+			competencies: ['Программирование', 'Автоматизация', 'Тестирование'],
+			imageUrl: 'https://via.placeholder.com/150',
+		},
+	];
+
+	const items = materials.map(material => (
+		<MaterialListItem
+			title={material.title}
+			author={material.author}
+			competencies={material.competencies}
+			imageUrl={material.imageUrl}
+		/>
 	));
+
 	return (
 		<div className={css.wrapper}>
-			MaterialList
-			{ items }
+			{items}
 		</div>
 	);
 };
--- src/pages/material-list/ui/material-list-block/list/item/MaterialListItem.tsx
+++ src/pages/material-list/ui/material-list-block/list/item/MaterialListItem.tsx
@@ -1,19 +1,31 @@
 import css from './MaterialListItem.module.scss';
 
-export const MaterialListItem = () => {
+interface MaterialListItemProps {
+	title: string;
+	author: string;
+	competencies: string[];
+	imageUrl: string;
+}
+
+export const MaterialListItem = ({
+	title, author, competencies, imageUrl, 
+}: MaterialListItemProps) => {
 	const a = 'a';
 	return (
 		<div className={css.wrapper}>
 			<div className={css.content}>
-				<div className={css.title}> Курс по созданию материалов</div>
-				<div className={css.author}> Создатель</div>
-				<div className={css.list}>
-					<div className={css.item}> Хаос</div>
-					<div className={css.item}> Разум</div>
-				</div>
+				<div className={css.title}>{title}</div>
+				<div className={css.author}>{author}</div>
+				{competencies && competencies.length > 0 && (
+					<div className={css.competencies}>
+						{competencies.map((competency) => (
+							<div key={competency} className={css.competency}>{competency}</div>
+						))}
+					</div>
+				)}
 			</div>
 			<div className={css.preview}>
-				<img src="https://i.pinimg.com/564x/34/f2/d5/34f2d554f45ac66757795e8bd0443abb.jpg" alt="alt" />
+				<img src={imageUrl} alt="" />
 			</div>
 		</div>
 	);
